80bc116ef3df3080df6f17e08782d94872efc337
Manoj Dayaram <m@noj.cc>
2022-10-06 20:51:56 -0700

    Init: Forking off of blank, mostly removing things.

---
+++ theme-variables.js
@@ -0,0 +1,16 @@
+/*
+ * Custom function used to generate the output of the theme variables; more about it can be found at https://getpublii.com/dev/theme-variables/
+ */
+
+var generateThemeVariables = function(params) {
+   let output = '';
+
+      output += ` 
+         :root {
+             --main-width:   ${params.mainWidth};
+         }`;  
+
+   return output;
+}
+
+module.exports = generateThemeVariables;
